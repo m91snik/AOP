3adca8c19f5e6ccf978e51b58b81bfa862a48a32
nikolay.garbuzov <g.m91snik@gmail.com>
2015-03-04 23:30:40 +0400

    clean up

--- src/main/java/com/m91snik/aspect/pointcut/ServicePointcut.java
+++ src/main/java/com/m91snik/aspect/pointcut/ServicePointcut.java
@@ -3,6 +3,8 @@
  */
 package com.m91snik.aspect.pointcut;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 /**
