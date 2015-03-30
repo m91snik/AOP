30fefd11d3f3b587d4cc71c479724fe075f3aec4
m91snik <g.m91snik@gmail.com>
2015-03-30 22:46:54 +0300

    refactoring

--- src/main/java/com/m91snik/aspect/staticcorsscutting/CompileValidationAspect.java
+++ src/main/java/com/m91snik/aspect/staticcorsscutting/CompileValidationAspect.java
@@ -4,7 +4,6 @@
 package com.m91snik.aspect.staticcorsscutting;
 
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.DeclareError;
 import org.aspectj.lang.annotation.DeclareWarning;
 
 /**
@@ -17,6 +16,6 @@ public class CompileValidationAspect {
     private static final String UNSAFE_USAGE_ERROR = "Usage of unsafe method!";
 
     //NOTE: it's possible to use DeclareError also to forbid compilation in case of rule violation
-    @DeclareError("get(* System.out) && within(com.m91snik.business.service..*)")
+    @DeclareWarning("get(* System.out) && within(com.m91snik.business.service..*)")
     private static final String DEFAULT_LOGGING_USAGE = "Use log4j instead of System.out!!";
 }
